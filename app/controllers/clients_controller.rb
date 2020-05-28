794beb2548a9545f3f3ae266384d75b52b671cac
neven <nevendrean@yahoo.fr>
2020-05-29 00:57:32 +0200

    kaminari clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,9 +4,9 @@ class ClientsController < ApplicationController
     # button that sends a dummay query string for the controller 
     def index
         if params[:c].present?
-            puts params[:c].present?
             @clients = Client.all.includes(comments: {resto: :genre})
             #@clients = Kaminari.paginate_array(@clients).page(params[:page]).per(3)
+            
             #render json: @clients.to_json
             render partial: 'clients/client', collection: @clients, layout: false
             #render partial: 'clients/client', locals:{clients: @clients}, layout: false EQUIVALENT
