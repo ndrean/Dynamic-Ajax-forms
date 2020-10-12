4bc2569c8543fad3f423bf7129773cf7182b5518
neven <nevendrean@yahoo.fr>
2020-10-12 02:06:32 +0200

    rm nginx

--- config/puma.rb
+++ config/puma.rb
@@ -8,7 +8,7 @@ min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port        3000 #ENV.fetch("PORT") { 3000 }
+port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 # environment ENV.fetch("RAILS_ENV") || "development"
