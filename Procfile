97431cc0d6080546c025a0cdcb57e6e8b0dfc54d
neven <nevendrean@yahoo.fr>
2020-10-12 13:14:46 +0200

    nginx

--- Procfile
+++ Procfile
@@ -2,5 +2,6 @@
 
 web: bin/start-nginx bundle exec puma -t 5:5  --config config/puma.rb
 
+# version without nginx: !! change puma.rb !!
 #web: bundle exec puma -t 5:5 -p ${PORT:-3000} 
 
--- config/puma.rb
+++ config/puma.rb
@@ -8,7 +8,7 @@ min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port        ENV.fetch("PORT") { 3000 }
+# port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 # environment ENV.fetch("RAILS_ENV") || "development"
@@ -22,7 +22,7 @@ rackup      DefaultRackup ##
 # app_dir =  File.expand_path("../..", __FILE__)
 
 # bind "unix://#{app_dir}/tmp/unix/sockets/nginx.socket"
-# listen "unix:///tmp/nginx.socket"
+listen "unix:///tmp/nginx.socket"
 
 workers     ENV.fetch('WEB_CONCURRENCY') { 2 }
 on_worker_fork { FileUtils.touch('/tmp/app-initialized') } ##
